799d3821f528864dec1e860bb50e54e4c4873288
Diana Ngo <dngo199@gmail.com>
2025-12-30 13:39:29 -0500

    finished part1a

---
+++ courseinfo/src/App.jsx
@@ -0,0 +1,59 @@
+const Header = ({course}) => {
+  return <h1>{course}</h1>
+}
+
+const Part = ({name, numberOfExercises}) => {
+  return (
+    <p>{name} {numberOfExercises}</p>
+  )
+}
+
+const Content = ({part1, exercises1, part2, exercises2, part3, exercises3}) => {
+  return (
+    <div>
+      <Part name={part1} numberOfExercises={exercises1}/>
+      <Part name={part2} numberOfExercises={exercises2}/>
+      <Part name={part3} numberOfExercises={exercises3}/>
+    </div>
+  )
+}
+
+const Total = ({exercises1, exercises2, exercises3}) => {
+  return (
+    <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+  );
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header 
+        course={course}
+      />
+      <Content 
+        part1={part1}
+        exercises1={exercises1}
+        part2={part2}
+        exercises2={exercises2}
+        part3={part3}
+        exercises3={exercises3}
+      />
+      <Total 
+        exercises1={exercises1}
+        exercises2={exercises2}
+        exercises3={exercises3}
+      />
+    </div>
+  )
+}
+
+
+export default App
