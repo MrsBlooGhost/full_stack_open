4c323c402a62e20f3fd4cc11321606d826702d19
Diana Ngo <dngo199@gmail.com>
2026-01-13 13:32:20 -0500

    notes example & part 3a: phonebook

---
+++ part3/phonebook/index.js
@@ -0,0 +1,93 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+let persons = [
+    { 
+      "id": "1",
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": "2",
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": "3",
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": "4",
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+];
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons);
+});
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = request.params.id;
+  const person = persons.find(person => person.id === id);
+
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+})
+
+app.get('/info', (request, response) => {
+  const now = new Date();
+  response.send(`
+    <p>Phonebook has info for ${persons.length} people</p>
+    <p>${now}</p>
+    `);
+});
+
+app.post('/api/persons', (request, response) => {
+  const name = request.body.name;
+  const number = request.body.number;
+
+  if (!name) {
+    return response
+      .status(400)
+      .json({ error: 'name is missing'});
+  }
+
+  if (!number) {
+    return response
+      .status(400)
+      .json({ error: 'number is missing'});
+  }
+
+  if (persons.find(person => person.name.toLowerCase() === name.toLowerCase())) {
+    return response
+      .status(400)
+      .json({ error: 'name must be unique'});
+  }
+
+  const person = {
+    id: Math.floor(Math.random() * 1000000000),
+    name,
+    number,
+  }
+
+  persons = persons.concat(person);
+  response.json(person);
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = request.params.id;
+  persons = persons.filter(person => person.id !== id);
+
+  response.status(204).end();
+})
+
+app.listen(PORT);
+console.log(`Server running on port ${PORT}`);
